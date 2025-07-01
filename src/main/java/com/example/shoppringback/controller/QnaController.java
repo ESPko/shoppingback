71ed4c945b3b9d2c728aa246f406bd8dab4b4918
ESPKO\Master <asddsa362@naver.com>
2025-07-02 01:59:37 +0900

    board qna로 전부 변경

--- src/main/java/com/example/shoppringback/controller/QnaController.java
+++ src/main/java/com/example/shoppringback/controller/QnaController.java
@@ -1,42 +1,42 @@
 package com.example.shoppringback.controller;
 
 import com.example.shoppringback.entity.QnA;
-import com.example.shoppringback.service.BoardService;
+import com.example.shoppringback.service.QnaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/board")
+@RequestMapping("/api/qna")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*") // 프론트와 연결 위해 CORS 허용
-public class BoardController {
+public class QnaController {
 
-    private final BoardService boardService;
+    private final QnaService qnaService;
 
     @GetMapping
     public List<QnA> getAllBoards() {
-        return boardService.findAll();
+        return qnaService.findAll();
     }
 
     @GetMapping("/{id}")
     public QnA getBoard(@PathVariable Long id) {
-        return boardService.findById(id);
+        return qnaService.findById(id);
     }
 
     @PostMapping
     public QnA createBoard(@RequestBody QnA qnA) {
-        return boardService.save(qnA);
+        return qnaService.save(qnA);
     }
 
     @PutMapping("/{id}")
     public QnA updateBoard(@PathVariable Long id, @RequestBody QnA qnA) {
-        return boardService.update(id, qnA);
+        return qnaService.update(id, qnA);
     }
 
     @DeleteMapping("/{id}")
     public void deleteBoard(@PathVariable Long id) {
-        boardService.delete(id);
+        qnaService.delete(id);
     }
 
 
--- src/main/java/com/example/shoppringback/service/QnaService.java
+++ src/main/java/com/example/shoppringback/service/QnaService.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BoardService {
+public class QnaService {
 
     private final QnaRepository qnaRepository;
 
