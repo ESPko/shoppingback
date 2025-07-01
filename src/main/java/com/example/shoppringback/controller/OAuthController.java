1c8a52e789ae39928a08670de79f7aaf27461aa3
JSY-JUNG <simp6219@naver.com>
2025-07-01 22:50:46 +0900

    no message

--- src/main/java/com/example/shoppringback/controller/OAuthController.java
+++ src/main/java/com/example/shoppringback/controller/OAuthController.java
@@ -54,6 +54,7 @@ public class OAuthController {
 
     @GetMapping("/naver")
     public ResponseEntity<?> naverLogin(@RequestParam String code, @RequestParam String state) {
+        System.out.println("ㅋㅋㄴㅇㅁㅇㅁㄴㅇ");
         try {
             // 1. 액세스 토큰 요청
             String accessToken = oAuthClient.getNaverAccessToken(code, state);
